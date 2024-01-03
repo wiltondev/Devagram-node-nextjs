b52f1dba785101be0a6d891c60eb258adfcd4ddb
wiltondev <92389600+wiltondev@users.noreply.github.com>
2024-01-03 20:16:58 -0300

    reverting

--- pages/api/publicacao.ts
+++ pages/api/publicacao.ts
@@ -1,128 +1,117 @@
 import sharp from 'sharp';
-<<<<<<< HEAD
 import type { NextApiResponse } from 'next';
 import type { RespostaPadraoMsg } from '../../types/RespostaPadraoMsg';
-import nc from "next-connect";
+import nc from 'next-connect';
 import { updload, uploadImagemCosmic, bucketDevagram } from '../../services/uploadImagemCosmic';
 import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
 import { PublicacaoModel } from '../../models/PublicacaoModel';
 import { UsuarioModel } from '../../models/UsuarioModel';
-=======
-import type {NextApiResponse} from 'next';
-import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
-import nc from 'next-connect';
-import {updload, uploadImagemCosmic, bucketDevagram} from '../../services/uploadImagemCosmic';
-import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
-import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
-import {PublicacaoModel} from '../../models/PublicacaoModel';
-import {UsuarioModel} from '../../models/UsuarioModel';
->>>>>>> parent of adb3a03 (vercel.json/politiCORS)
 import { politicaCORS } from '../../middlewares/politicaCORS';
-import {getVideoDurationInSeconds} from 'get-video-duration';
+import { getVideoDurationInSeconds } from 'get-video-duration';
 import { Readable } from "stream";
 
 
 
 const handler = nc()
-    .use(updload.single('file'))
-    .post(async (req : any, res : NextApiResponse<RespostaPadraoMsg>) => {
-        try{
-            const {userId} = req.query;
-            const usuario = await UsuarioModel.findById(userId);
-            if(!usuario){
-                return res.status(400).json({erro : 'Usuario nao encontrado'});
-            }
-
-            if(!req || !req.body){
-                return res.status(400).json({erro : 'Parametros de entrada nao informados'});
-            }
-            const {descricao} = req?.body;
-
-            if(!descricao || descricao.length < 2 || descricao.length > 20){
-                return res.status(400).json({erro : 'Descricao nao e valida'});
-            }
-    
-            if(!req.file || !req.file.originalname){
-                return res.status(400).json({erro : 'Publique uma foto ou um video'});
-            }
-
-            const fileExtension = req.file.originalname.toLowerCase().slice(-4);
-            const allowedImageExtensions = [".jpeg", ".png", ".jpg", ".bmp"];
-            const allowedVideoExtensions = [".mp4", ".webm", ".mov", ".avi"];
-      
-            // Verifique se o formato do arquivo é suportado (imagem ou vídeo)
-            if (
-              !allowedImageExtensions.includes(fileExtension) &&
-              !allowedVideoExtensions.includes(fileExtension)
-            ) {
-              return res.status(400).json({
-                erro: "Formato de arquivo não suportado. Apenas imagens ou vídeos são permitidos.",
-              });
-            }
-      
-            if (allowedVideoExtensions.includes(fileExtension)) {
-              
-              
-              const videoStream = Readable.from(req.file.buffer);
-      
-              // Use a biblioteca get-video-duration para obter a duração do vídeo
-              const durationInSeconds = await getVideoDurationInSeconds(videoStream);
-      
-              if (durationInSeconds > 120) {
-                return res
-                  .status(400)
-                  .json({ erro: "Vídeo deve ter no máximo 2 minutos de duração." });
-              }
-              
-
-
-
-            } else if (allowedImageExtensions.includes(fileExtension)) {
-              const imageBuffer = req.file.buffer;
-      
-              // Use a biblioteca sharp para obter informações sobre a imagem
-              const imageInfo = await sharp(imageBuffer).metadata();
-      
-              if (!imageInfo) {
-                return res.status(400).json({
-                  erro: "Não foi possível obter informações sobre a imagem.",
-                });
-              }
-      
-              
-            }
-            
-      
-        
-            const media = await uploadImagemCosmic(req);
-      
-            let publicacao ={
-              idUsuario: usuario._id,
-              descricao,
-              url: media?.media?.url,
-              mediaId: media?.media?.id, 
-              data: new Date(),
-            };
-
-            usuario.publicacoes++;
-            await UsuarioModel.findByIdAndUpdate({_id : usuario._id}, usuario);
-
-            await PublicacaoModel.create(publicacao);
-            return res.status(200).json({msg : 'Publicacao criada com sucesso'});
-        }catch(e){
-            console.log(e);
-            return res.status(400).json({erro : 'Erro ao cadastrar publicacao'});
-        }
-})
- .put(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
+  .use(updload.single('file'))
+  .post(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
     try {
-      const {userId, publicacaoId } = req.query;
+      const { userId } = req.query;
+      const usuario = await UsuarioModel.findById(userId);
+      if (!usuario) {
+        return res.status(400).json({ erro: 'Usuario nao encontrado' });
+      }
+
+      if (!req || !req.body) {
+        return res.status(400).json({ erro: 'Parametros de entrada nao informados' });
+      }
+      const { descricao } = req?.body;
+
+      if (!descricao || descricao.length < 2 || descricao.length > 20) {
+        return res.status(400).json({ erro: 'Descricao nao e valida' });
+      }
+
+      if (!req.file || !req.file.originalname) {
+        return res.status(400).json({ erro: 'Publique uma foto ou um video' });
+      }
+
+      const fileExtension = req.file.originalname.toLowerCase().slice(-4);
+      const allowedImageExtensions = [".jpeg", ".png", ".jpg", ".bmp"];
+      const allowedVideoExtensions = [".mp4", ".webm", ".mov", ".avi"];
+
+      // Verifique se o formato do arquivo é suportado (imagem ou vídeo)
+      if (
+        !allowedImageExtensions.includes(fileExtension) &&
+        !allowedVideoExtensions.includes(fileExtension)
+      ) {
+        return res.status(400).json({
+          erro: "Formato de arquivo não suportado. Apenas imagens ou vídeos são permitidos.",
+        });
+      }
+
+      if (allowedVideoExtensions.includes(fileExtension)) {
+
+
+        const videoStream = Readable.from(req.file.buffer);
+
+        // Use a biblioteca get-video-duration para obter a duração do vídeo
+        const durationInSeconds = await getVideoDurationInSeconds(videoStream);
+
+        if (durationInSeconds > 120) {
+          return res
+            .status(400)
+            .json({ erro: "Vídeo deve ter no máximo 2 minutos de duração." });
+        }
+
+
+
+
+      } else if (allowedImageExtensions.includes(fileExtension)) {
+        const imageBuffer = req.file.buffer;
+
+        // Use a biblioteca sharp para obter informações sobre a imagem
+        const imageInfo = await sharp(imageBuffer).metadata();
+
+        if (!imageInfo) {
+          return res.status(400).json({
+            erro: "Não foi possível obter informações sobre a imagem.",
+          });
+        }
+
+
+      }
+
+
+
+      const media = await uploadImagemCosmic(req);
+
+      let publicacao = {
+        idUsuario: usuario._id,
+        descricao,
+        url: media?.media?.url,
+        mediaId: media?.media?.id,
+        data: new Date(),
+      };
+
+      usuario.publicacoes++;
+      await UsuarioModel.findByIdAndUpdate({ _id: usuario._id }, usuario);
+
+      await PublicacaoModel.create(publicacao);
+      return res.status(200).json({ msg: 'Publicacao criada com sucesso' });
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json({ erro: 'Erro ao cadastrar publicacao' });
+    }
+  })
+  /*.put(async (req: any, res: NextApiResponse<RespostaPadraoMsg>) => {
+    try {
+      const { userId, publicacaoId } = req.query;
 
       const usuario = await UsuarioModel.findById(userId);
 
-      if(!usuario){
-        return res.status(400).json({erro : 'Usuario nao encontrado'});
+      if (!usuario) {
+        return res.status(400).json({ erro: 'Usuario nao encontrado' });
       }
 
 
@@ -204,12 +193,12 @@ const handler = nc()
       console.error(e);
       return res.status(400).json({ erro: "Erro ao atualizar publicação" });
     }
-  });
+  });*/
 
 export const config = {
-    api : {
-        bodyParser : false
-    }
+  api: {
+    bodyParser: false
+  }
 }
 
-export default politicaCORS(validarTokenJWT(conectarMongoDB(handler))) ;
+export default politicaCORS(validarTokenJWT(conectarMongoDB(handler)));
