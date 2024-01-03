883ff49a20c117c8f617c37d2b5bc3398954ec0f
wiltondev <92389600+wiltondev@users.noreply.github.com>
2024-01-02 22:10:29 -0300

    Revert "vercel.json/politiCORS"
    This reverts commit adb3a038cc0dfa580e7c38b192b7828f4c049e34.

--- middlewares/politicaCORS.ts
+++ middlewares/politicaCORS.ts
@@ -7,7 +7,7 @@ export const politicaCORS = (handler : NextApiHandler) =>
     try{
         await NextCors(req, res, {
             origin : '*',
-            methods : ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+            methods : ['GET', 'POST', 'PUT'],
             optionsSuccessStatus : 200, // navegadores antigos dao problema quando se retorna 204
         });
 
